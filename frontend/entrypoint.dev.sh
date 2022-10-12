be2f9444632cf7ff0ab464f4778bc22153a13fcc
Jakob Notland <jakob.notland@ntnu.no>
2022-10-12 14:04:55 +0200

    Add some info at put the Dockerfile to working state for production.

--- frontend/entrypoint.dev.sh
+++ frontend/entrypoint.dev.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-# Code to run after building the image
+# Code to run after building the image.
+# This script runs the application in dev mode. 
+# The application will watch for changes in the code.
+# This script does not run in production mode.
 
 PORT=5000 npm start
